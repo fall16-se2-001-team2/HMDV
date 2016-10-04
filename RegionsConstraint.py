7360e07ccb5dc06f67151bc01c0501e0a90a8b8e
Isaac Styles <isaacstyles92@gmail.com>
2016-10-04 08:13:03 -0400

    added stub method __repr__() for serializing constraints in database

--- RegionsConstraint.py
+++ RegionsConstraint.py
@@ -3,3 +3,9 @@ class RegionsConstraint:
 
     def __init__(self):
         constraints = []
+    #--------------------------------------------
+    #__repr__()
+    #returns an unambiguous string that represents the particular constraint
+    #--------------------------------------------
+    def __repr__(self):
+        return str
