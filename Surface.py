b922764cb4270c409ab88dec7ab3d25e4f7b389f
Isaac Styles <isaacstyles92@gmail.com>
2016-10-04 08:16:19 -0400

    added discretize(), which samples the surface of a map at equidistance points

--- Surface.py
+++ Surface.py
@@ -1,11 +1,26 @@
 import math
 class Surface:
-    delta = .001 #distance to sample the statistical surface
-    zValues = [[]]  #height of the map at the discrete sample point
-#constructor Surface(float, float)
-    #inputs:
+    delta = .001                                                    #distance to sample the statistical surface
+                                                                    #  this needs to be determined by the ratio of the represented geographical region to the pixel resolution
+    zValues = [[]]                                                  # height of the map for the set of discrete sample points
+
+    #--------------------------------------------
+    #constructor Surface(float, float)
+    #inputs: (longitudal width, latitudal height
+    #--------------------------------------------
     def __init__(self, mapWidth, mapHeight):
-        sampleWidth =  math.ceil(mapWidth / self.delta) #int of sample points across the x axis
-        sampleHeight = mapHeight / self.delta #int of sample points across the y axis
-        zValues = [[0 for x in range(sampleWidth)] for y in range(sampleHeight)]
-        zValues[x][y] = self.calcZ()
+        sampleWidth =  math.ceil(mapWidth / self.delta)             #int of sample points across the x axis
+        sampleHeight = mapHeight / self.delta                       #int of sample points across the y axis
+        zValues = [[0 for x in range(sampleWidth)] for y in range(sampleHeight)]    #contains the results of the calculation for each pixel
+        pass
+
+    #--------------------------------------------
+    #discretize()
+    #sample the surface for each pixel
+    #inputs: (longitudal width, latitudal height
+    #--------------------------------------------
+    def discretize(self):
+        for x in range (self.sampleWidth):                           #calculate the height at each descrete point in the array
+            for y in range (self.sampleHeight):
+                self.zValues[x][y] = self.calcZ(x*self.delta,y*self.delta)   #the height of the map at (long,lat)
+        pass
