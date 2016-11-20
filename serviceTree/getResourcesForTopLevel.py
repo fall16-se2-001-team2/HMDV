3840d1d08539df4c854ff56043df660544fb8f8c
Matthew Humphrey <humphreymg@goldmail.etsu.edu>
2016-11-19 20:21:11 -0500

    Fixed the addTopLevel class to work correctly

---
+++ serviceTree/getResourcesForTopLevel.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Matthew Humphrey
+"""
+import requests
+import re
+import json
+import codecs
+from bs4 import BeautifulSoup
+
+def main():
+    #inp = input("What is the name of the top level service you wish to search for?")
+    inp = "Not Found"
+    res = getResourcesForTopLevel(inp)
+    for r in res:
+        print(r)
+
+def getResourcesForTopLevel(topLevelName):
+    f = codecs.open('../data/resource.json', 'r', 'utf-8')
+    resourceList = json.load(f)
+    f.close()
+
+    resources = []
+
+    for r in resourceList:
+        if r["topLevelServices"].__contains__(topLevelName):
+            resources.append(r)
+
+    #k = codecs.open('../data/resourceOf#ServiceType#.json','w','utf-8')
+    #k.write("[")
+    #for t in topLevels:
+    #    names["name"] = t
+    #    json.dump(names,k)
+    #    k.write(",\n")
+    #k.write("]")
+    return resources
+
+
+if __name__ == "__main__":
+    main()
