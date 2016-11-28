23e895fd6f6a999bfc5c4ec61c5150dc057b23da
Isaac Styles <styles@etsu.edu>
2016-11-28 02:51:09 -0500

    final GUI update

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------------------------------------
 #
-# Name                     :Isaac Styles
+# Name                     :Isaac Styles and Ryan Meades
 # Department Name : Computer and Information Sciences
 # File Name                :Main.py
 # Purpose                  :Create a map of a general (top level) service, and approximate the number of people
@@ -14,7 +14,7 @@
 #
 #-----------------------------------------------------------------------------------------------------------
 #
-# Modified Date	: Nov 27, 2016
+# Modified Date	: Nov 28, 2016
 # Modified By		: Isaac Styles
 #
 #-------------------------------------------------------------------------------------------------------------
@@ -23,7 +23,7 @@
 from ProviderList import ProviderList
 from serviceTree import getTopLevelNames
 from Map import Map
-
+defaultHeight = 18          #constant declaring the default height of provider, out of 100
 def main():
     '''retrieve the general resource types for this particular dataset. NOTE this doesn't change with the resource file.
         The serviceTree scripts would have to build another tree to conform to another dataset.'''
@@ -40,11 +40,14 @@ def main():
     sys.exit(app.exec_())
     #app.exec_()
 
-
+#-------------------------------------------------------------
+# gui()
+# purpose: accept a user inputted top level service and radius
+#-------------------------------------------------------------
 def gui():
     from PyQt4 import QtCore, QtGui
     import sys
-    try:
+    try:                        #the following code is for potential translation to other languages, and is QT generated
         _fromUtf8 = QtCore.QString.fromUtf8
     except AttributeError:
         def _fromUtf8(s):
@@ -58,6 +61,10 @@ def gui():
         def _translate(context, text, disambig):
             return QtGui.QApplication.translate(context, text, disambig)
 
+    # ------------------------------------------------------------
+    # UI_MainWindow
+    # purpose: display the main GUI window
+    # ------------------------------------------------------------
     class Ui_MainWindow(object):
         def setupUi(self, MainWindow, topLevels):
             MainWindow.setObjectName(_fromUtf8("MainWindow"))
@@ -78,7 +85,7 @@ def gui():
 
             # Instruction Text Browser
             self.textBrowser = QtGui.QTextBrowser(self.centralwidget)
-            self.textBrowser.setGeometry(QtCore.QRect(20, 10, 341, 131))
+            self.textBrowser.setGeometry(QtCore.QRect(20, 10, 341, 350))
             self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
 
             # Radius Input from user
@@ -124,32 +131,42 @@ def gui():
             MainWindow.setStatusBar(self.statusbar)
             self.menubar.addAction(self.menuMap_View_Vers_0_01.menuAction())
 
-            self.retranslateUi(MainWindow, topLevels)
+            self.retranslateUi(MainWindow)
             QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        # ------------------------------------------------------------
+        # handleOpenMap()
+        # purpose: listener that produces a map once triggered
+        # ------------------------------------------------------------
         def handleOpenMap(self):
             produceMap(self.dropDownList.currentText(), self.RadiusEntry1Obj.text())
-
-        def retranslateUi(self, MainWindow, topLevels):
-            MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        # ------------------------------------------------------------
+        # retranslateUi()
+        # purpose: modifies the GUI objects once they're created
+        # ------------------------------------------------------------
+        def retranslateUi(self, MainWindow):
+            MainWindow.setWindowTitle(_translate("MainWindow", "ETSU Social Services Mapper", None))
             self.ExitProgramObj.setText(_translate("MainWindow", "Exit", None))
             self.textBrowser.setHtml(_translate("MainWindow",
                                                 "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
                                                 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
                                                 "p, li { white-space: pre-wrap; }\n"
                                                 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">Team Pandas Map Creator<br /><br />To generate a map, enter a radius on the right and click &quot;Generate Map.&quot;</span></p></body></html>",
+                                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">ETSU Social Work Department<br /><br /></p><p>To generate a map, enter a default radius and select the type of service. Click &quot;Generate Map&quot; to continue.</span></p><p><img src=panda.jpg>Team Pandas</p></body></html>",
                                                 None))
-            self.RadiusEntry1Obj.setText(_translate("MainWindow", "0", None))
+            self.RadiusEntry1Obj.setText(_translate("MainWindow", "25", None))
             self.label.setText(_translate("MainWindow", "Radius (mi.)", None))
             self.GenerateMapObj.setText(_translate("MainWindow", "Generate Map", None))
-            self.menuMap_View_Vers_0_01.setTitle(_translate("MainWindow", "Map View Vers. 0.01", None))
+            self.menuMap_View_Vers_0_01.setTitle(_translate("MainWindow", "Map Maker v.1.0", None))
 
         def close_application(self):
             sys.exit()
 
     return QtGui, Ui_MainWindow
-
+#------------------------------------------------------------
+# produceMap(string, string)
+# purpose: take user input and produce a corresponding map
+#------------------------------------------------------------
 def produceMap(topLevel, radius):
     '''scripts to modify the resource file without producing a map. included for performance reasons during testing'''
     # Parser.parseCounties("data/counties_TN.json", "leaflet/counties-tn.js")    #parse the US Census geoJSON into one supported by leaflet
@@ -160,12 +177,13 @@ def produceMap(topLevel, radius):
     providersList = ProviderList('data/resourceLatLon.json', topLevel, radius, 1000)
     map = Map(topLevel)  # initialize map with title of selected top level service
     for provider in providersList.providers:
-        map.addProvider(provider.latitude, provider.longitude, 18, provider.getRadius(), str(provider))
+        map.addProvider(provider.latitude, provider.longitude, defaultHeight, provider.getRadius(), str(provider))
     '''the following line is an example using pandas iterable arrays to place heat points in Folium'''
     # map.add_children(plugins.HeatMap([[providerCoords[0], providerCoords[1]] for name, row in morning_rush.iloc[:1000].iterrows()]))
     map.save('tempBrowseLocal.html')  # save the generated map to html
     import webbrowser, os.path
     webbrowser.open("file:///" + os.path.abspath('tempBrowseLocal.html'))  # display the map in the browser
+
     '''the following db code is the beginnings of a way to associate an output map with variables(radius, height), demographics files, and county boundaries.'''
     # sqlite_file = 'research.sqlite'     # name of the sqlite database file
     # cursor = initializeDB(sqlite_file)  #cursor is pointer to db connection
@@ -174,7 +192,6 @@ def produceMap(topLevel, radius):
     # browseLocal(contents)              #display webpage
     # cursor.close()
     '''this code tests the age constraint parser, which is incomplete'''
-    # c = Curve()        #intended to fit a curve to the age demographics data acquired from the census. This was never used
     # from PopConstraint import PopConstraint
     # PopConstraint.ageConstraint("children from 8-14 years old")
 
