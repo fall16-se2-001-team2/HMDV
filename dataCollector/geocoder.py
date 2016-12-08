20303c202c44e0e0021c2e82634d4751cefbdfb5
Isaac Styles <isaacstyles92@gmail.com>
2016-12-08 17:19:34 -0500

    catch geocoder timeout, and fail soft. added geocoder logger

--- dataCollector/geocoder.py
+++ dataCollector/geocoder.py
@@ -19,7 +19,7 @@
 import json
 import codecs
 from geopy import geocoders
-
+from geopy.exc import GeocoderTimedOut
 class Geocoder:
     api_key="AIzaSyBHJYku7O6cGgMNVL3nXo9dPB0EkgMVlXM" # gmaps api key for ETSU Computing Dept. listed under styles@etsu.edu (Isaac Styles)
     def __init__(self):
@@ -34,21 +34,30 @@ class Geocoder:
         return lat, lon
     '''addLatLonJSON is a script to add lat and lon to a whole JSON file.'''
     @staticmethod
-    def addLatLonJSON(source, dest, max=2):
-        g = geocoders.GoogleV3(api_key=Geocoder.api_key)  # gmaps api key for ETSU Computing Dept. listed under styles@etsu.edu (Isaac Styles)
+    def addLatLonJSON(source, dest, max=0):
+        g = geocoders.GoogleV3(api_key=Geocoder.api_key,timeout=10)  # gmaps api key for ETSU Computing Dept. listed under styles@etsu.edu (Isaac Styles)
         with codecs.open(source, 'r', 'utf-8') as inf:
             resourceList = json.load(inf)
         li = codecs.open(dest, 'w', 'utf-8')
         li.write('[')
         count = 0
-        for resource in resourceList:
-            place, (lat, lon) = g.geocode(resource["address"])      #place is not necessary
-            resource['latitude'] = lat
-            resource['longitude'] = lon
-            json.dump(resource,li)
-            count += 1
-            if count == len(resourceList) or count == max:
-                break
-            li.write(',\n')
+        import time, sys
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        with codecs.open(timestr + ".log", 'w', 'utf-8') as log:
+            for resource in resourceList:
+                try:
+                    place, (lat, lon) = g.geocode(resource["address"])      #place is not necessary
+                    resource['latitude'] = lat
+                    resource['longitude'] = lon
+                except GeocoderTimedOut as e:
+                    log.write("Error: geocode failed on input "+ resource["address"] +" with message " + e.message)
+                except:
+                    log.write("GENERAL error message: " + str(sys.exc_info()[0]))
+
+                json.dump(resource,li)
+                count += 1
+                if count == len(resourceList) or count == max:
+                    break
+                li.write(',\n')
         li.write(']')
         li.close()
