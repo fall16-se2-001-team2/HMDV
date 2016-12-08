d1ff727c159b1af6fa72231c00b6fee7175e1416
Isaac Styles <isaacstyles92@gmail.com>
2016-12-08 17:21:29 -0500

    script to append geocode data to resource file

---
+++ dataCollector/Script.py
@@ -0,0 +1,3 @@
+from dataCollector import Geocoder
+
+Geocoder.Geocoder.addLatLonJSON('../data/resource.json','../data/resourceLatLon2.json')   #manually add lat and lon to the resource file for 10 providers
